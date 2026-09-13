637e86e3dbe14b229fc347cca8949afeef22167c
elkhalifte <39893327+elkhalifte@users.noreply.github.com>
2026-09-13 01:13:26 +0100

    Update chime_bell_text.dart

--- lib/src/effects/chime_bell_text.dart
+++ lib/src/effects/chime_bell_text.dart
@@ -14,6 +14,9 @@ class ChimeBellText extends StatelessWidget {
   /// The style to apply to the text
   final TextStyle? style;
 
+  /// Auto animate when widget is first created
+  final bool autoPlay;
+  
   /// The text alignment
   final TextAlign textAlign;
 
@@ -27,6 +30,7 @@ class ChimeBellText extends StatelessWidget {
     super.key,
     required this.text,
     this.style,
+    this.autoPlay = true,
     this.textAlign = TextAlign.start,
     required this.config,
     this.onControllerCreated,
@@ -37,6 +41,7 @@ class ChimeBellText extends StatelessWidget {
     return AnimatedTextBase(
       text: text,
       style: style,
+      autoPlay: autoPlay,
       textAlign: textAlign,
       config: config,
       onControllerCreated: onControllerCreated,
