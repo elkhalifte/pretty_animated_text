b52c194f282fc3edd78b909e0a3a7887922a9be2
elkhalifte <39893327+elkhalifte@users.noreply.github.com>
2026-09-13 01:17:40 +0100

    Update offset_text.dart

--- lib/src/effects/offset_text.dart
+++ lib/src/effects/offset_text.dart
@@ -13,6 +13,9 @@ class OffsetText extends StatelessWidget {
   /// The style to apply to the text
   final TextStyle? style;
 
+  /// Auto animate when widget is first created
+  final bool autoPlay;
+
   /// The text alignment
   final TextAlign textAlign;
 
@@ -30,6 +33,7 @@ class OffsetText extends StatelessWidget {
     super.key,
     required this.text,
     this.style,
+    this.autoPlay = true,
     this.textAlign = TextAlign.start,
     required this.config,
     this.slideType = SlideAnimationType.topBottom,
@@ -41,6 +45,7 @@ class OffsetText extends StatelessWidget {
     return AnimatedTextBase(
       text: text,
       style: style,
+      autoPlay: autoPlay,
       textAlign: textAlign,
       config: config,
       onControllerCreated: onControllerCreated,
